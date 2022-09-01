30f1a40b9f363cf39baebc9407fa0d97f7c6a2ea
Peyton Harrison <peyton006@hotmail.com>
2022-09-01 16:23:10 -0500

    commit tree 4c11dc900898171c2ba6c956c77a107d8612012b

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative './user'
+
+class Student < User
+
+    attr_accessor :wisdom
+
+    def initialize(wisdom)
+        @wisdom = wisdom
+        @knowledge = []
+    end
+
+    def learn
+        @knowledge << wisdom
+    end
 
 end
+
+s1 = Student.new("Eating lunch is important")
